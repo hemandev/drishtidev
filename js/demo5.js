77a88cbbacf297cc96bd3464d080cdef8a0c07aa
nabeel_kabeer <nabeel.k035@gmail.com>
2018-01-12 19:20:44 +0530

    minor changes

--- js/demo5.js
+++ js/demo5.js
@@ -195,10 +195,10 @@
     };
 
     sl = new Slideshow(document.querySelector('.slideshow'));
-	/*var elem = document.getElementsByClassName("demo-5")[0];
+	var elem = document.getElementsByClassName("demo-5")[0];
 			var hammer = new Hammer(elem);
 			hammer.get('swipe').set({
-			direction: Hammer.DIRECTION_ALL,
+			direction: Hammer.DIRECTION_HORIZONTAL,
 			threshold: 5,
 			velocity: 0.1
 			});
@@ -208,6 +208,6 @@
 			} else if (event.type === "swiperight") {
 				sl.navigate('prev');
 			}
-		})*/
+		})
     imagesLoaded('.slide__img', { background: true }, () => document.body.classList.remove('loading'));
 };
